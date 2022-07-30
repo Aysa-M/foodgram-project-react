7de63a28b56fd07022d85b93bd4328aa81b33d33
Aysa Matsakova <happiness390@gmail.com>
2022-07-30 22:35:03 +0300

    loading

--- foodgram/load_data.py
+++ foodgram/load_data.py
@@ -11,7 +11,7 @@ from recipes.models import Ingredient
 path = '.'
 os.chdir(path)
 
-
+# loading ingredients from csv file
 with open('ingredients.csv', 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file, fieldnames=['name', 'measurement_unit'])
     to_db = (Ingredient(
