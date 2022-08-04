19faa9458c553784959973ba01ca298e503be0ff
Aysa Matsakova <happiness390@gmail.com>
2022-08-04 21:18:14 +0300

    pep8

--- foodgram/api/views.py
+++ foodgram/api/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum, Exists, OuterRef, Value
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, status, viewsets
